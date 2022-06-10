6e90a67655c6e449db86c8c74568fd9e578bd4a4
Arkag7 <96461374+Arkag7@users.noreply.github.com>
2022-06-10 21:23:08 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask,render_template, request
+import numpy as np
+import pickle
+
+model = pickle.load(open('insurence.pkl',"rb"))
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("home.html")
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    final_feat = list(request.form.values())
+    #final_Feat = [i for i in final_feat]
+    final_feat = np.array(final_feat).reshape(1,6)
+
+    output = model.predict(final_feat)[0]
+
+    return render_template("predict.html",output=output)
+
+if __name__ =="__main__":
+    app.run(debug=True)
